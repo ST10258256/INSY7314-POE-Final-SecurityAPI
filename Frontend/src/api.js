5f7cf394ce70d068d63cc1420da08916fffe3104
lukej <lukejordan2004@gmail.com>
2025-10-24 13:19:22 +0200

    added /api to base url

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -8,7 +8,7 @@ import axios from "axios";
  *   baseURL: "https://securityapi-x4rg.onrender.com/api"
  */
 const API = axios.create({
-  baseURL: "https://insy7314-poe-final-securityapi.onrender.com", 
+  baseURL: "https://insy7314-poe-final-securityapi.onrender.com/api", 
   headers: { "Content-Type": "application/json" },
 });
 
