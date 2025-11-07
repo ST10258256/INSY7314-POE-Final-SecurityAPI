303027c294b21037285cb782ba23b2eddc2ef9b9
lukej <lukejordan2004@gmail.com>
2025-11-07 17:44:34 +0200

    Trying to solve the 12 last issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar";
-
-// Post pages are kept in repository but their imports/routes are commented out.
-// Uncomment the imports and routes below to re-enable them.
-// import PostList from "./components/PostList";
-// import PostCreate from "./components/PostCreate";
-// import PostEdit from "./components/PostEdit";
-
 import Register from "./components/register";
 import Login from "./components/login";
 import ForgotPassword from "./pages/ForgotPassword";
-//import ResetPassword from "./pages/ResetPassword";
 import Dashboard from "./pages/Dashboard";
 import PaymentForm from "./pages/PaymentForm";
 import ProtectedRoute from "./routes/ProtectedRoute";
