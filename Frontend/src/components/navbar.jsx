ab2ca7cc41bf24df7155639c782e043521c16948
lukej <lukejordan2004@gmail.com>
2025-11-07 22:16:46 +0200

    Changed authRole so there are no clashes

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import { getUserRole } from "../utils/auth"; 
+import { getUserRole } from "../utils/authRole"; 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function hasValidToken() {
